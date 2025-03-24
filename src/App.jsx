acdfb4bf66a62f2afa06c3dcaefc56e131b62bfe
Jay-boy2007 <jayb34585@gmail.com>
2025-03-24 17:20:42 +0100

    Page Added: About Us

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-<<<<<<< HEAD
 import Header from "./components/Header";
 import About from "./pages/About";
-=======
-// import Header from "./components/Header";
-// import Home from "./pages/Home";
-// import ServicesPage from "./pages/ServicePage";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// function App() {
-//   return (
-//     <Router>
-//       <Header />
-//       {/* <Home /> */}
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/services/:categorySlug" element={<ServicesPage />} />
-//       </Routes>
-
-//       {/* Other components */}
-//     </Router>
-//   );
-// }
-
-// export default App;
-import Header from "./components/Header"; //
->>>>>>> 49d58d55b8169637f88ce3d6fa6fb19e3bc5f51e
 import Home from "./pages/Home";
 import ServicesPage from "./pages/ServicePage";
 import SignupCustomer from "./pages/SignupCustomer";
