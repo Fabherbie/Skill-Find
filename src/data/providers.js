388871fe2d7b8560008105873cd8efdce4673127
folorunso-Akeem/Folorunso <folorunsoakeem20@gmail.com>
2025-04-07 14:23:23 +0100

    Auth Pages Added: login, signupCustomer, signuprovider

---
+++ src/data/providers.js
@@ -0,0 +1,16 @@
+export const providers = [{
+        ProviderName: "Ade",
+        role: "Manager",
+        category: "Business Owner",
+    },
+    {
+        ProviderName: "Kola",
+        role: "Freelancer",
+        category: "contractor",
+    },
+    {
+        ProviderName: "example",
+        role: "Manager",
+        category: "Vendor",
+    },
+]
---
+++ src/data/users.js
@@ -0,0 +1,13 @@
+export const users = [{
+        userName: "AK",
+        role: "customer"
+    },
+    {
+        userName: "Example Provider",
+        role: "service-provider"
+    },
+    {
+        userName: "Exampple Customer",
+        role: "customer"
+    },
+]
